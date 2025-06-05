6a87f4fb0601cb4c10ac7baf99dc78883ccdf678
mh-shihan <mahmodul.no.1@gmail.com>
2025-06-05 23:37:12 +0600

    Add student model and interfaces for user and guardian details

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+export type Guardian = {
+  fatherName: string;
+  fatherDesignation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherDesignation: string;
+  motherContactNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  name: UserName;
+  gender: "male" | "female";
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: "active" | "inactive";
+};
