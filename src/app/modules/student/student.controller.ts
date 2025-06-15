a88b68633e62747c7f5a9f3eb1d75b1da3197bae
mh-shihan <mahmodul.no.1@gmail.com>
2025-06-15 21:53:18 +0600

    Add Joi validation for student data and enhance error handling in createStudent

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
+import studentJoiJoiValidationSchema from './student.validation';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
     const { student: studentData } = req.body;
 
-    const result = await StudentServices.createStudentIntoDB(studentData);
+    const { error, value } =
+      studentJoiJoiValidationSchema.validate(studentData);
+
+    const result = await StudentServices.createStudentIntoDB(value);
+
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: 'Something Went Wrong!',
+        error: error,
+      });
+    }
+
     if (result) {
       res.status(200).json({
         success: true,
