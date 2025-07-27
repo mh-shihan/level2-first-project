cd176a5528a6cce883aff5e2826a0b65c465a89f
mh-shihan <mahmodul.no.1@gmail.com>
2025-07-27 15:43:39 +0600

    Created Faculty Interface

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,24 @@
+import { Types } from 'mongoose';
+import { TBloodGroup, TGender, TUserName } from '../../interface/module';
+
+export type TFaculty = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+};
+
+// export interface FacultyModel extends Model<TFaculty>{
+//     isUser
+// }
