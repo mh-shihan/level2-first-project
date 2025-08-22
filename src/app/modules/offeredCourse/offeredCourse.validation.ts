a0e6f9abc10cfffb647de169a27b2b2e26127db1
mh-shihan <mahmodul.no.1@gmail.com>
2025-08-22 10:28:09 +0600

    Add time validation functions and integrate into offered course validation

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,19 +1,38 @@
 import { z } from 'zod/v4';
-import { Days } from './offeredCourse.constant';
+import {
+  Days,
+  invalidTimeFormat,
+  isValid24HourTime,
+} from './offeredCourse.constant';
 
 const createOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    section: z.number(),
-    maxCapacity: z.number(),
-    days: z.array(z.enum([...Days])),
-    startTime: z.string(), // HH: MM   00-23: 00-59
-    endTime: z.string(),
-  }),
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      section: z.number(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum([...Days])),
+      startTime: z
+        .string()
+        .refine(isValid24HourTime, { message: invalidTimeFormat }), // HH: MM   00-23: 00-59
+      endTime: z
+        .string()
+        .refine(isValid24HourTime, { message: invalidTimeFormat }),
+    })
+    .refine(
+      ({ startTime, endTime }) => {
+        const start = new Date(`1970-01-01T${startTime}:00`);
+        const end = new Date(`1970-01-01T${endTime}:00`);
+        return end > start;
+      },
+      {
+        message: 'Start time should be before End time!',
+      },
+    ),
 });
 
 const updateOfferedCourseValidationSchema = z.object({
